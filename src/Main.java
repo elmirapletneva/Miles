6e2c61c9b9e25cc33f850fd292844a90835c4e9a
ElmiraPletneva <elmirapletneva@yandex.ru>
2022-09-08 19:12:24 +0300

    New calculation

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int price = 100; // стоимость билета
-        int bonus = 20; // кол-во рублей за 1 бонусную милю
-        System.out.println(price / bonus+"-бонусов получит покупатель за покупку");
+        int bonus = price / 20;
+        System.out.println(bonus + "-бонусов получит покупатель за покупку");
 
     }
 
